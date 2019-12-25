13945a83166d24867769cbfd74caff0336b06635
Lê Tiến Chiến <chien.le@tiki.vn>
2019-12-25 15:10:24 +0700

    find hub feature

--- server/main.go
+++ server/main.go
@@ -12,12 +12,45 @@ import (
 var hubs = make(map[string]*Hub)
 var upgrader = websocket.Upgrader{}
 
+func findHubHandler(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("Finding hub")
+
+	conn, _ := upgrader.Upgrade(w, r, nil)
+
+	var key string
+
+	for k, v := range hubs {
+
+		if len(v.players) < 2 { // hub not enough players
+
+			key = k
+			break
+		}
+	}
+
+	hub, ok := hubs[key]
+
+	if !ok {
+		log.Println("No available hub:", key, conn.RemoteAddr())
+		conn.WriteMessage(websocket.CloseMessage, []byte{})
+		return
+	}
+
+	var s = socket.InitSocket(conn, hub)
+	hub.register <- &s
+
+	go s.Read()
+	go s.Write()
+}
+
 func createHubHandler(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("Starting new hub")
 
 	conn, _ := upgrader.Upgrade(w, r, nil)
 
+	// TODO: generate random key
 	key := "asdfasdf"
 
 	_, ok := hubs[key]
@@ -71,6 +104,7 @@ func main() {
 
 	http.HandleFunc("/create_hub", createHubHandler)
 	http.HandleFunc("/join_hub", joinHubHandler)
+	http.HandleFunc("/find_hub", joinHubHandler)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Welcome to caro-online")
 	})
